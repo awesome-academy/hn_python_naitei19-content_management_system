18f7dc4c99ed5d74fde6d178987bad3e9fb6b710
LongDT205096 <dinhthanhlong0@gmail.com>
2023-09-18 12:18:24 +0700

    task 68311

--- CMS/home/urls.py
+++ CMS/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = [ 
+    path('', views.home, name='home'),
     path('user/<int:pk>/', views.showUserDetail, name='user'),
     path('userUpdate/<int:pk>/', views.updateUser, name='userUpdate'),
+    path('like/<int:pk>/', views.likeArticle, name='likeArticle'),
 ]
--- CMS/home/views.py
+++ CMS/home/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
 
 # Create your views here.
 
-from . models import User
+from . models import User, Article, Like
 from . forms import UserForm
 
+#Home page
+def home(request):
+    articles = Article.objects.all().order_by("id")
+    context = {'articles': articles}
+
+    return render(request, 'home.html', context)
+
+
+#Customize user information
 def showUserDetail(request, pk):
     user = get_object_or_404(User, id=pk)
     context = {'user': user}
@@ -24,3 +34,22 @@ def updateUser(request, pk):
     context = {'form': form}
 
     return render(request, 'home/user_update.html', context)
+
+#Like and rate article
+def likeArticle(request, pk):
+    article = get_object_or_404(Article, id=pk)
+    user = get_object_or_404(User, id=1)
+    
+    if request.method == 'POST':
+        if not Like.objects.filter(user=user, article=article).exists():
+            Like.objects.create(user=user, article=article)
+        else:
+            Like.objects.filter(user=user, article=article).delete()
+
+        context = {
+            'likes': article.count_likes(),
+        }
+
+        return JsonResponse(context, safe=False)
+    
+    return redirect('home')
