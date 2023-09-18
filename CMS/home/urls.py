14a44acd197580ddfff7e0e304a5b2c0f781e3e4
LongDT205096 <dinhthanhlong0@gmail.com>
2023-09-18 12:18:24 +0700

    task 68311

--- CMS/home/urls.py
+++ CMS/home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('user/<int:pk>/', views.showUserDetail, name='user'),
     path('userUpdate/<int:pk>/', views.updateUser, name='userUpdate'),
+    path('like/<int:pk>/', views.likeArticle, name='likeArticle'),
 ]
