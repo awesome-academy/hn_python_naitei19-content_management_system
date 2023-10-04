b6e9b363011011dda4ff1abd2d357040c8acd31a
Drag Stark <kdragtstark@gmail.com>
2023-10-05 04:33:38 +0700

    New Account Email Verification and Send Email reset password

--- CMS/CMS/settings.py
+++ CMS/CMS/settings.py
@@ -149,3 +149,12 @@ AUTH_USER_MODEL = 'home.User'
 
 MEDIA_ROOT = BASE_DIR + 'media'
 MEDIA_URL = '/media/'
+
+# Emailing settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST'), 
+EMAIL_FROM = os.environ.get('EMAIL_FROM'), 
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER'), 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD'), 
+EMAIL_PORT = os.environ.get('EMAIL_PORT'), 
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS'), 
